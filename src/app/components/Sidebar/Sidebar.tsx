1663d17d366f1c84e09324f03459d52cc7275e12
KanterIV <126972573+KanterIV@users.noreply.github.com>
2025-08-03 13:37:49 +0200

    next navigation (link + useRouter)

--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -1,8 +1,17 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
 import SidebarItem from '../SidebarItem/SidebarItem';
+import { usePathname, useRouter } from 'next/navigation';
 
 export default function Sidebar() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleExitClick = () => {
+    router.push('/');
+  };
   return (
     <aside className="fixed top-0 left-0 z-40 w-60 h-screen">
       <div className="flex flex-col h-full overflow-y-auto bg-gray-900">
@@ -15,19 +24,23 @@ export default function Sidebar() {
         />
         <ul className="space-y-7">
           <SidebarItem
+            current={pathname === '/dashboard'}
             pathname="/dashboard"
             src="/icons/squares.svg"
             alt="dashboard icon">
             Dashboard
           </SidebarItem>
           <SidebarItem
+            current={pathname === '/companies'}
             pathname="/companies"
             src="/icons/briefcase.svg"
             alt="companies icon">
             Companies
           </SidebarItem>
         </ul>
-        <button className="flex items-center gap-2 p-6 mt-auto mx-auto">
+        <button
+          className="flex items-center gap-2 p-6 mt-auto mx-auto cursor-pointer"
+          onClick={handleExitClick}>
           <Image
             width={18}
             height={18}
